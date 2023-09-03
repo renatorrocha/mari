c1a91594ecf536c65ab40c8f04ea448140ecba0d
Renatodev <renatorrodrigues2002@gmail.com>
2023-09-03 17:47:10 -0300

    ajuste de botao erro

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ function App() {
   
   return (
     <>
+            <Typography variant='h1'>Oi Amor (Maria Clara),</Typography>
         <Typography variant='h1'>Quer Casar comigo Amor?</Typography>
-        <Button variant="contained" size='large' color='success' sx={{margin:2}}>Eu quero</Button>
-        <Button variant="outlined" size='large' color='error'>Eu te odeio, morra!</Button>
+        <Button variant="contained" size='large' color='success' sx={{margin:8}} onClick={()=>{ alert('Parabens amor agora vc esta casada comigo (Renato Rocha'); }}>Eu quero</Button>
+        <Button variant="outlined" size='large' color='error' onClick={()=>{ window.open("about:blank", "_self");
+window.close();}}>Eu te odeio, morra!</Button>
     </>
   )
 }
