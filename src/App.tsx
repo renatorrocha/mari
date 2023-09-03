8458d01dfb7af47b85deb6441da4ed5852c990bc
Renatodev <renatorrodrigues2002@gmail.com>
2023-09-03 18:19:06 -0300

    ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { useState } from 'react'
+
 import './App.css'
 import { Typography, Button } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    
           <>
             <Typography variant='h1'>Oi Amor (Maria Clara),</Typography>
-        <Typography variant='h1'>Quer Casar comigo Amor?</Typography>
-        <Button variant="contained" size='large' color='success' sx={{margin:8}} onClick={()=>{ alert('Parabens amor agora vc esta casada comigo (Renato Rocha'); }}>Eu quero</Button>
+        <Typography variant='h1'>♥ Quer Casar comigo Amor ?♥</Typography>
+        <Button variant="contained" size='large' color='success' sx={{margin:8}} onClick={()=>{ alert('Parabens amor agora vc esta casada comigo (Renato Rocha) Eu te amo muito♥'); }}>Eu quero</Button>
         <Button variant="outlined" size='large' color='error' onClick={()=>{ window.open("about:blank", "_self");
 window.close();}}>Eu te odeio, morra!</Button>
     </>
-    </>
+  
   )
 }
 
 export default App
+ 
